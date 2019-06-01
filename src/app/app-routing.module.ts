9fad8e7839bd350c3c4338c72c75648ea974c3f8
David Rodriguez <david._98@outlook.com>
2019-06-02 01:00:26 +0200

    añadidas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ArticuloComponent } from './pages/articulo/articulo.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'home', component: PortafolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'articulo', component: ArticuloComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  // forRoot se usa para las rutas de la raiz de la app
+  // para rutas hijas se usa forChild
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
